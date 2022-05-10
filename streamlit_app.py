016727745be0f2f376fa793bcf53178b43afc0fa
Andrew Nicoll <62946360+anicoll44@users.noreply.github.com>
2022-05-10 11:35:58 -0400

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import pytrends
+import pandas as pd
+import numpy as np
+import requests
+from GoogleNews import GoogleNews
+
+kw_list = st.text_input('Enter a Keyword', '')
+
+# build request parameters
+timeframe = "now 7-d"
+number_of_related_queries = 3
+
+#build model
+pytrend = TrendReq()
+pytrend.build_payload(kw_list, cat=0, timeframe = timeframe, geo = 'US')
+
+#get top related queries for provided keyword
+related_queries = pytrend.related_queries()
+related_queries.values()
+
+#build kw list that includes provided and related top queries
+top_df = list(related_queries.values())[0]['top'].head(number_of_related_queries)
+top_related_list = top_df['query'].tolist()
+kw_list.extend(top_related_list)
+
+#get interest over time
+pytrend.build_payload(kw_list, cat=0, timeframe = timeframe, geo = 'US')
+interest_o_time_df = pytrend.interest_over_time()
+interest_o_time_df = interest_o_time_df.drop(columns=['isPartial'])
+
+st.line_chart(data=interest_o_time_df)
