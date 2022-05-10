e263f676fa8f46112b2d120395a0a0e3a8ab218b
Andrew Nicoll <62946360+anicoll44@users.noreply.github.com>
2022-05-10 17:36:27 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -89,7 +89,7 @@ if get_data_button:
   interest_o_time_df = pytrends.interest_over_time()
   interest_o_time_df = interest_o_time_df.drop(columns=['isPartial'])
   
-  st.caption('Interest Over Time')
+  st.sub_header('Interest Over Time')
   st.line_chart(data=interest_o_time_df)
   
   #get rising queries
@@ -97,5 +97,5 @@ if get_data_button:
   rising_df = rising_df.reset_index(drop=True)
   rising_df = rising_df.rename(columns={"query": "Keyword", "value": "% Increase"})
   
-  st.caption('Rising Related Keywords')
+  st.sub_header('Rising Related Keywords')
   st.dataframe(data=rising_df)
