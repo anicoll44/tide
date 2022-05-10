ef397021c1b88a5c71d81a6aeec81304f97f4d6c
Andrew Nicoll <62946360+anicoll44@users.noreply.github.com>
2022-05-10 12:08:28 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,23 +9,25 @@ from GoogleNews import GoogleNews
 # build request parameters
 timeframe = "now 7-d"
 number_of_related_queries = 3
-kw_list = ["Blockchain"]
+kw = st.text_input('Enter a Keyword', None, type = 'default')
+kw_list = [kw]
 
-pytrends = TrendReq()
-pytrends.build_payload(kw_list, cat=0, timeframe = timeframe, geo = 'US')
+if kw is not None:
+  pytrends = TrendReq()
+  pytrends.build_payload(kw_list, cat=0, timeframe = timeframe, geo = 'US')
 
-#get top related queries for provided keyword
-related_queries = pytrends.related_queries()
-related_queries.values()
+  #get top related queries for provided keyword
+  related_queries = pytrends.related_queries()
+  related_queries.values()
 
-#build kw list that includes provided and related top queries
-top_df = list(related_queries.values())[0]['top'].head(number_of_related_queries)
-top_related_list = top_df['query'].tolist()
-kw_list.extend(top_related_list)
+  #build kw list that includes provided and related top queries
+  top_df = list(related_queries.values())[0]['top'].head(number_of_related_queries)
+  top_related_list = top_df['query'].tolist()
+  kw_list.extend(top_related_list)
 
-#get interest over time
-pytrends.build_payload(kw_list, cat=0, timeframe = timeframe, geo = 'US')
-interest_o_time_df = pytrends.interest_over_time()
-interest_o_time_df = interest_o_time_df.drop(columns=['isPartial'])
+  #get interest over time
+  pytrends.build_payload(kw_list, cat=0, timeframe = timeframe, geo = 'US')
+  interest_o_time_df = pytrends.interest_over_time()
+  interest_o_time_df = interest_o_time_df.drop(columns=['isPartial'])
 
-st.line_chart(data=interest_o_time_df)
+  st.line_chart(data=interest_o_time_df)
