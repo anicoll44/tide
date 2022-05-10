0996fe6f644ff11638ece102ff997d827e3d9098
Andrew Nicoll <62946360+anicoll44@users.noreply.github.com>
2022-05-10 17:31:25 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -93,4 +93,6 @@ if get_data_button:
   
   #get rising queries
   rising_df = list(related_queries.values())[0]['rising']
+  rising_df = rising_df.reset_index(drop=True)
+  rising_df.rename(columns={"query": "Keyword", "value": "% Increase"})
   st.dataframe(data=rising_df)
