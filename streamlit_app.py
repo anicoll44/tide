2f25d33fe079b5e6cb00e17e02f768a7ccff7b08
Andrew Nicoll <62946360+anicoll44@users.noreply.github.com>
2022-05-11 11:04:24 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -64,13 +64,15 @@ elif rising_query_timeframe == 'Past 5 Years':
   rising_query_timeframe = 'today 5-y'
 
 #Select number of related queries
-number_of_related_queries = st.sidebar.slider('# of Related Keywords', min_value=0, max_value=4, value=4, help='The number of top related keywords to return and compare against your target keyword')
+number_of_related_queries = st.sidebar.slider('# of Related Keywords', min_value=0, max_value=4, value=2, help='The number of top related keywords to return and compare against your target keyword')
+
+#Select number of related news
+number_of_related_news = st.sidebar.slider('# of Related News Articles', min_value=0, max_value=5, value= 2, help='The number of related news articles to return for your target and related keywords')
 
 #Button to trigger getting google trends data
 get_data_button = st.sidebar.button('Get Google Trends Data')
 
 #Get Google trends data
-@st.cache(suppress_st_warning=True)
 if get_data_button:
   pytrends = TrendReq()
   pytrends.build_payload(kw_list, cat=0, timeframe = timeframe, geo = 'US')
@@ -89,7 +91,7 @@ if get_data_button:
   interest_o_time_df = pytrends.interest_over_time()
   interest_o_time_df = interest_o_time_df.drop(columns=['isPartial'])
   
-  st.markdown('##### Interest Over Time')
+  st.markdown('#### Interest Over Time')
   st.line_chart(data=interest_o_time_df)
      
   col1, col2, col3 = st.columns(3)
@@ -99,8 +101,8 @@ if get_data_button:
      rising_df = list(related_queries.values())[0]['rising']
      rising_df = rising_df.reset_index(drop=True)
      rising_df = rising_df.rename(columns={"query": "Keyword", "value": "% Increase"})
+     top_rising_list = rising_df['Keyword'].head(number_of_related_news).tolist()
 
-     st.text("")
      st.markdown('###### Rising Related Keywords')
      st.dataframe(data=rising_df)
      
@@ -108,8 +110,8 @@ if get_data_button:
      real_time_trends = pytrends.realtime_trending_searches(pn='US')
      real_time_trends = real_time_trends.drop('entityNames', axis = 1)
      real_time_trends = real_time_trends.rename(columns={"title": "Topic"})
-
-     st.text("")                                           
+     real_time_list = real_time_trends['Topic'].head(number_of_related_news).tolist()
+                                                
      st.markdown('###### Realtime Search Trends (US)')
      st.dataframe(data=real_time_trends)
 
@@ -117,30 +119,36 @@ if get_data_button:
      trending_df = pytrends.trending_searches(pn='united_states')
      trending_df = trending_df.rename(columns={0: "Keyword"})
      trending_df['Rank'] = trending_df.index + 1
+     trending_list = trending_df['Keyword'].head(number_of_related_news).tolist()
      
-     st.text("")
      st.markdown('###### Daily Search Trends (US)')
      st.dataframe(data=trending_df)
   
   #Get Google News Data
   googlenews = GoogleNews()
+  
+  #Create df to load news data
+  news_df = pd.DataFrame(columns=['title', 'media',	'date',	'datetime',	'desc',	'link',	'img', 'query'])
+
+  #Create list of uniquq tracked keywords and related queries
+  gnews_list = kw_list + top_rising_list + trending_list + real_time_list
+  gset = set(gnews_list)
+  gnews_list = gset
 
   #Get Google News for each item in list
-  st.text("")
-  with st.form("Google News Form"):
-      st.markdown('###### Google News')
-      news_kw = st.text_input('Enter a Keyword', '', type = 'default', help = 'Get news from Google for the keyword entered')
-      get_news_button = st.form_submit_button('Check Google News')
-
-      if get_news_button:
-        try:
+  for item in gnews_list:
+      try:
           googlenews.clear()
-          googlenews.search(news_kw)
+          googlenews.search(item)
           result = googlenews.results()
-          news_df = pd.DataFrame(result)
-        except requests.exceptions.Timeout:
+          temp_news_df = pd.DataFrame(result)
+          temp_news_df['query'] = item
+          news_df = news_df.append(temp_news_df, ignore_index=True)
+      except requests.exceptions.Timeout:
           st.write('Timeout occured, please try again')
   
-        #Cleanup and show news df
-        news_df = news_df.drop('img', axis = 1)
-        st.dataframe(data=news_df)
+  #Cleanup and show news df
+  news_df = news_df.drop('img', axis = 1)
+  news_df = news_df.drop_duplicates()
+  st.markdown('###### Related News')
+  st.dataframe(data=news_df)
